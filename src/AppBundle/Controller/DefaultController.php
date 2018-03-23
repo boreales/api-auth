6559c45f8693e0a75f07b9a7c9cfca48967f4100
lorweb <boreales.creations@gmail.com>
2018-03-23 10:48:33 +0100

    Adding logout system

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -34,26 +34,59 @@ class DefaultController extends Controller
     $user = $em->getRepository('AppBundle:User')->findOneByUsername($username);
 
     if(!$user){
-      return 0;
+      return new Response(0); //User doesn't exists
     }
 
     $encoder = $this->get('security.password_encoder');
     $isPasswordValid = $encoder->isPasswordValid($user, $pass);
 
     if($isPasswordValid){
+      $tokens = $em->getRepository('AppBundle:AuthToken')->findByUser($user);
+
+      //Remove old tokens
+      if($tokens != null){
+        foreach($tokens as $token){
+          $em->remove($token);
+        }
+      }
+
       $authToken = new AuthToken();
       $authToken->setValue(base64_encode(random_bytes(50)));
       $authToken->setCreatedAt(new \DateTime('now'));
       $authToken->setUser($user);
-
       $em->persist($authToken);
-      $em->flush();
-      var_dump($authToken->getValue());
 
-      return $authToken;
+      $user->setLastLogin(new \DateTime('now'));
+      $em->persist($user);
+
+      $em->flush();
+
+      return $authToken; //Your X-Auth-Token
     }
 
-    return 1;
+    return new Response(1); //Invalid password
   }
 
+  /**
+   * @Rest\Get("/auth-break")
+   */
+  public function authBreakAction(Request $request)
+  {
+    $token = $request->headers->get('X-Auth-Token');
+    if($token != null){
+      $em = $this->get('doctrine.orm.entity_manager');
+      $auth = $em->getRepository('AppBundle:AuthToken')->findOneByValue($token);
+      if($auth != null){
+        $em->remove($auth);
+        $em->flush();
+
+        return new Response(1); //Token removed
+      }
+      else{
+        return new Response(2); //Token not found
+      }
+    }
+
+    return new Response(0); //Token doesn't exists
+  }
 }
